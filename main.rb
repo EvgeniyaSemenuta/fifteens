e2d9d2cbd8369306b17b23447ebe592d26b11525
Janny <uspex-uspet@yandex.ru>
2012-06-25 15:18:21 +0300

    Created fill game field function.

--- main.rb
+++ main.rb
@@ -5,19 +5,7 @@ class Game
     start_array = (1..15).to_a
     start_array << 100
     start_array = start_array.shuffle
-    @game_field = []
-    i=0
-    k=0
-    while i<@w
-      j=0
-      @game_field[i] = []
-      while j<@w
-        @game_field[i][j] = start_array[k]
-        k +=1
-        j +=1
-      end
-      i +=1
-    end
+    fill_game_field start_array
     print
   end
 
@@ -41,20 +29,15 @@ class Game
       end
       i +=1
     end
+    p n_i
+    p n_j
+    p empt_i
+    p empt_j
 
     if n_j == empt_j
-      if n_i>empt_i
-        razn = n_i - empt_i
-       for k in empt_i..n_i-1
-         @game_field[k][n_j] = @game_field[k+1][n_j]
-       end
-      else
-        razn = empt_i - n_i
-        empt_i.downto(n_i-1) do |k| @game_field[k][n_j] = @game_field[k-1][n_j] end
-      end
-      @game_field[n_i][n_j] = 100
-    #elseif n_i == empt_i
-     # shift n_j empt_js
+      shift n_i, empt_i, n_j, empt_j
+    elseif n_i == empt_i
+      shift n_j, empt_j, n_i, empt_i
         else
           puts "Wrong number!"
     end
@@ -70,7 +53,7 @@ class Game
     end
   end
 
-  def shift n_i, empt_i
+  def shift n_i, empt_i, n_j, empt_j
     if n_i>empt_i
         razn = n_i - empt_i
        for k in empt_i..n_i-1
@@ -81,6 +64,22 @@ class Game
         empt_i.downto(n_i-1) do |k| @game_field[k][n_j] = @game_field[k-1][n_j] end
     end
       @game_field[n_i][n_j] = 100
+  end
+
+  def fill_game_field start_array
+    @game_field = []
+    i=0
+    k=0
+    while i<@w
+      j=0
+      @game_field[i] = []
+      while j<@w
+        @game_field[i][j] = start_array[k]
+        k +=1
+        j +=1
+      end
+      i +=1
+    end
   end
 
 end
