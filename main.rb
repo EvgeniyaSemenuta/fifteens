3cc8c21836e4052a0c500ef0b20f092ce75b8a69
Janny <uspex-uspet@yandex.ru>
2012-06-25 14:54:29 +0300

    Added vertical shift by game field.

--- main.rb
+++ main.rb
@@ -1,17 +1,17 @@
 class Game
 
   def init
-    w=4
+    @w=4
     start_array = (1..15).to_a
     start_array << 100
     start_array = start_array.shuffle
     @game_field = []
     i=0
     k=0
-    while i<w
+    while i<@w
       j=0
       @game_field[i] = []
-      while j<w
+      while j<@w
         @game_field[i][j] = start_array[k]
         k +=1
         j +=1
@@ -21,11 +21,66 @@ class Game
     print
   end
 
+  def start_play
+    puts "Please, enter the number"
+    number = gets.chomp.to_i
+
+    i=0
+    while i<@w
+      j=0
+      while j<@w
+        if number==@game_field[i][j]
+          n_i = i
+          n_j = j
+        end
+        if @game_field[i][j]==100
+          empt_i = i
+          empt_j = j
+        end
+        j +=1
+      end
+      i +=1
+    end
+
+    if n_j == empt_j
+      if n_i>empt_i
+        razn = n_i - empt_i
+       for k in empt_i..n_i-1
+         @game_field[k][n_j] = @game_field[k+1][n_j]
+       end
+      else
+        razn = empt_i - n_i
+        empt_i.downto(n_i-1) do |k| @game_field[k][n_j] = @game_field[k-1][n_j] end
+      end
+      @game_field[n_i][n_j] = 100
+    #elseif n_i == empt_i
+     # shift n_j empt_js
+        else
+          puts "Wrong number!"
+    end
+
+    print
+  end
+
+  private
+
   def print
     @game_field.each do |cell|
         puts cell.join("\t")
     end
   end
 
+  def shift n_i, empt_i
+    if n_i>empt_i
+        razn = n_i - empt_i
+       for k in empt_i..n_i-1
+         @game_field[k][n_j] = @game_field[k+1][n_j]
+       end
+      else
+        razn = empt_i - n_i
+        empt_i.downto(n_i-1) do |k| @game_field[k][n_j] = @game_field[k-1][n_j] end
+    end
+      @game_field[n_i][n_j] = 100
+  end
 
 end
